6d02b7b2f454f5b0a1794d711876209d397ebe89
0jsm0 <martinsjulio113@gmail.com>
2024-02-28 11:26:37 -0500

    As métricas do utils foram arrumadas, agr só falta fazer o script onde chama as funçoes e printa

--- utils.py
+++ utils.py
@@ -1,18 +1,22 @@
 import numpy as np
-#no código do absrel tá faltando divir M que é o total de pixels
+
 def calculate_absrel(gt_data, pred_data):
-    # Calcular AbsRel
+    
     absrel = np.mean(np.abs(gt_data - pred_data) / gt_data)
     
     return absrel
 
-#no código do delta adicionar linhas para calcular a proporção por de false e verdadeiro por dado
+
 def calculate_delta(gt_data, pred_data):
     # Calcular Delta
     max_ratio = np.maximum(gt_data / pred_data, pred_data / gt_data)
     delta = max_ratio < 1.25
 
-    return delta
+    # Contar quantos valores resultaram em True
+    num_true_values = np.count_nonzero(delta)
+    measures = num_true_values/ gt_data.size
+
+    return delta, measures
 
 
 
