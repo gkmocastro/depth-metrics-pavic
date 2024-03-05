cde0b59a5c4256fa12ceed8dba0963df0f5eee80
0jsm0 <martinsjulio113@gmail.com>
2024-03-05 11:32:07 -0500

    script atualizado

--- script.py
+++ script.py
@@ -5,15 +5,17 @@ from utils import (
          calculate_absrel,
          calculate_delta,)
 
-
-depth_path = "/home/julio/Desktop/depth_npy"
+# As preds nesse código estão no formato npy
+pred_path = "Caminho da predição"
 ds = load_dataset("sayakpaul/nyu_depth_v2", split="validation")
 all_absrel = []
 all_delta = []
 # Iterar sobre os dados e calcular as métricas para cada par de deltas
 for idx, sample in enumerate(ds):
     # Carregar os deltas
-    delta1 = np.load(os.path.join(depth_path, f"{idx + 1}_pred.npy"))
+    # delta1 = pred e delta2 = gt
+    # O delta começa de 0 e vai até 653, por isso se a pred começar com 1 o ideal é somar 1 ao idx, como feito abaixo
+    delta1 = np.load(os.path.join(pred_path, f"{idx + 1}_pred.npy"))
     delta2 = sample["depth_map"]
     
     # Calcular as métricas
